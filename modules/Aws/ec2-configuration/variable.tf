c3e2a9e9da58649f72a88059d1185fc7ded9ba84
akash <akashwaghmode07@gmail.com>
2024-07-08 15:02:23 +0530

    ec2

---
+++ modules/Aws/ec2-configuration/variable.tf
@@ -0,0 +1,20 @@
+# variables.tf
+variable "aws_region" {
+  description = "The AWS region to create resources in"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of instance to create"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "key_name" {
+  description = "The key name for the EC2 instance"
+  type        = string
+}
