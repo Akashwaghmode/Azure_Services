92be597f122f1fe95aaf6b9d64074e13887e2ce8
akash <akashwaghmode07@gmail.com>
2024-07-08 15:50:09 +0530

    vpc

---
+++ modules/Aws/vpc-configuration/variable.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+
+}
+
+variable "subnet_cidr_block" {
+  description = "CIDR block for the subnet"
+
+}
+
+variable "availability_zone" {
+  description = "Availability Zone for subnet
+}
+
+variable "vpc_name" {
+  description = "Name for the VPC"
+
+}
