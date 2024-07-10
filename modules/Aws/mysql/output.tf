272b4dd99d010102f72b494ef5fa5f281738ab3f
Akash dhanaji Waghmode <98980675+Akashwaghmode@users.noreply.github.com>
2024-07-10 17:04:53 +0530

    Delete modules/Aws/mysql directory

--- modules/Aws/mysql/output.tf
+++
@@ -1,9 +0,0 @@
-output "db_endpoint" {
-  description = "The endpoint of the RDS instance"
-  value       = aws_db_instance.mysql_instance.endpoint
-}
-
-output "db_instance_id" {
-  description = "The ID of the RDS instance"
-  value       = aws_db_instance.mysql_instance.id
-}
